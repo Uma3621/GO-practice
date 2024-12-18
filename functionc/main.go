bc9ed21dd84b5db4afc3ccf7f3d814cfe901a3d3
Uma3621 <umayasalapu3621@gmail.com>
2024-12-18 17:10:55 -0600

    withparam

--- functionc/main.go
+++ functionc/main.go
@@ -9,10 +9,14 @@ const (
 	appname     = "uma"      // it's only inside this package as it's starting with lower case
 )
 
+func withparam(p1 string, p2 int) {
+	fmt.Println("with parameters ::"+p1, p2)
+}
 func main() {
 	const name = "prathyusha"
 	const num = 21 //we can declare and initialize conct without mentioning any type
 	fmt.Println("this is a fucntion")
 	fmt.Println(appname)
 	fmt.Println(DATE_FORMAT)
+	withparam("hi", 66)
 }
