cb5fbb28f3c581af7a6b9c2f9d818b0e7439a6b9
Uma3621 <umayasalapu3621@gmail.com>
2024-12-30 10:29:27 -0600

    added module package

---
+++ functionc/modules/testmodules.go
@@ -0,0 +1,16 @@
+package modules
+
+import "fmt"
+
+func Vals() (int, int) {
+	return 5, 6
+}
+func multipleReturnValues() {
+
+	a, b := Vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := Vals()
+	fmt.Println(c)
+}
