001d8ac3301b68b59d18093d45da2b989ba8a955
Uma3621 <umayasalapu3621@gmail.com>
2025-01-02 15:07:37 -0600

    Using range on Map

--- functionc/Range/RangeUsing.go
+++ functionc/Range/RangeUsing.go
@@ -14,8 +14,25 @@ func Range() {
 	fmt.Println("sum:", sum)
 
 	for i, num := range nums {
+
 		if num == 3 {
 			fmt.Println("index:", i)
 		}
 	}
+	//range on map iterates over key/value pairs.
+	//Initializes a map named kvs with string keys and string values.
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
 }
